943e2e2ee4bcbc9a70a4c5414bfa92501e264e80
bhask <bhask@DESKTOP-87G4C0A>
2017-01-26 12:41:20 +0530

    test

--- JUnitTest/src/test/java/com/junit/test/StringHelperTestParam.java
+++ JUnitTest/src/test/java/com/junit/test/StringHelperTestParam.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-@RunWith(Parameterized.class)
+@RunWith(Parameterized.class) 
 public class StringHelperTestParam {
 	private StringHelper helprObj = new StringHelper();
 	private String in,out;
